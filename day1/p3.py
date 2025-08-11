a2df381419dfa60694a2421a4d2e9477b69c86ce
diyaballal <diyaballal3127@gmail.com>
2025-08-11 10:58:48 +0530

    These are the day 1 programs

---
+++ day1/p3.py
@@ -0,0 +1,5 @@
+#Check if the given integer number is Even.
+input_number=int(input("Enter a number to check if it is Even:"))
+
+if input_number % 2 == 0:
+    print("The given number",input_number,"is even")
