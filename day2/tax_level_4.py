114e206cbd5af45e4306d2081454ac2d5a23352e
diyaballal <diyaballal3127@gmail.com>
2025-08-13 16:19:22 +0530

    These are the changes done at the end of day 3

--- day2/tax_level_4.py
+++ day2/tax_level_4.py
@@ -1,3 +1,6 @@
-import tax_level_3 as t3,tax_level_1 as tax_level_1
+import tax_level3 as t3, tax_level1 as t1
 
-net_annual_salary=t1.gross_anual_salary
+net_annual_salary = t1.gross_annual_salary - t3.total_tax_amount
+print(f'Annual Gross Salary = {t1.gross_annual_salary}')
+print(f'Total Tax Amount = {t3.total_tax_amount}')
+print(f'Net Annual Salary = {net_annual_salary}')
---
+++ day3/range.py
@@ -0,0 +1,7 @@
+num = 10
+
+for i in range(num):
+    print(f'i = {i}')
+    i += 2
+
+#Here the output will simply show i=0, i=1,...., i=10
